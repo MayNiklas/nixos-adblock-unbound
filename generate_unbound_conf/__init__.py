c7d74b3710acbfaaabd5a62115c2403483d2efdf
MayNiklas <info@niklas-steffen.de>
2022-06-13 17:07:18 +0200

    create file reader

--- generate_unbound_conf/__init__.py
+++ generate_unbound_conf/__init__.py
@@ -1,8 +1,24 @@
+import argparse
+import os
+
 from .generate_unbound_conf import main
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--adlist', type=file_path)
+    return parser.parse_args()
+
+
+def file_path(string):
+    if os.path.isfile(string):
+        return os.path.abspath(string)
+    else:
+        raise FileNotFoundError(string)
 
 
 def main():
     """ Entrypoint """
 
-    # Get the current working directory
-    generate_unbound_conf.main()
+    args = parse_arguments()
+    generate_unbound_conf.main(args)
--- generate_unbound_conf/generate_unbound_conf.py
+++ generate_unbound_conf/generate_unbound_conf.py
@@ -1,6 +1,18 @@
-def main():
+import os
+from time import sleep
+
+
+def main(args):
     """
-    sort files in a directory by date
+    generate unbound config for adblocking
     """
 
-    print("Hello world!")
+    reader = open(args.adlist, 'r')
+    adlist = reader.readlines()
+    reader.close
+
+    for line in adlist:
+        print(line.strip())
+
+        # delay
+        sleep(0.1)
