2760bfb59564ee4e73abace6bc0b6eb1a4d66e3e
MayNiklas <info@niklas-steffen.de>
2022-06-14 16:58:38 +0200

    output correct format

--- cmd/convert_to_unbound/main.go
+++ cmd/convert_to_unbound/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 )
 
 var (
@@ -13,14 +14,28 @@ var (
 
 func Run() {
 
+	// parsing -adlist flag
+	// -adlist contains the file name of the adlist
+	// it defaults to hosts
 	flag.Parse()
-	log.Println("converting: " + *adlist)
 
+	// reading the adlist
 	data, err := ioutil.ReadFile(*adlist)
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
 	}
-	fmt.Println("Contents of file:", string(data))
+
+	// going through the adlist line by line
+	for _, line := range strings.Split(string(data), "\n") {
+		log.Println(line)
+
+		// if line begins with "0.0.0.0" it is a valid line
+		if strings.HasPrefix(line, "0.0.0.0") {
+			// format the line into the unbound format
+			fmt.Println(strings.Replace(line, "0.0.0.0 ", "local-zone: \"", 1) + "\" static")
+		}
+
+	}
 
 }
